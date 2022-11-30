53c2c86ee6c4c85422c9bf6d71e92943f9c94a81
Ivan Turasov <turasov.ivan@gmail.com>
2022-11-30 21:44:14 +0100

    1mlp: Add naive forward propagation FC

--- 1-fnn/mlp-components/fc.py
+++ 1-fnn/mlp-components/fc.py
@@ -1,14 +1,29 @@
 import numpy as np
+from collections.abc import Callable
 
 class FC:
     '''
         Fully connected layer implementation - Ivan Turasov
     '''
+    total_fc_num = 0
 
-    def __init__(self, in_features: int, out_features: int) -> FC:
+    def __init__(self, num_in_ft: int, num_out_ft: int, activation: Callable[[np.array], np.array]):
         '''
-            wm - weight matrix of size (in_features x out_features)
-            bs - vector of all neurons' biases
+            wm - weight matrix of size (num_in_ft x num_out_ft)
+            bs - vector of all neurons' biases (size = num_out_ft)
         '''
-        self.wm = np.random.default_rng(1).random((in_features, out_features))
-        self.bs = np.zeros(out_features)
+        total_fc_num += 1 # Keep track of total fc layers number
+
+        self.num_in_ft = num_in_ft
+        self.num_out_ft = num_out_ft
+        self.activation = activation # Activation function
+        self.wm = np.random.default_rng(1).random((num_in_ft, num_out_ft))
+        self.bs = np.zeros(1, num_out_ft)
+
+    def fw(self, in_ft: np.array) -> np.array:
+        ''' Apply weights and activation function '''
+        out = np.dot(in_ft, self.wm) + self.bs
+        return self.activation(out)
+    
+    def bw(self):
+        pass
