431bc5bb67aea844e3affd117899a8e0d23dbfab
Ivan Turasov <turasov.ivan@gmail.com>
2022-12-13 19:56:05 +0100

    1mlp: Add activation derivatives and some tests

--- 1-fnn/mlp-components/activation.py
+++ 1-fnn/mlp-components/activation.py
@@ -1,37 +1,90 @@
 import numpy as np
+import torch
 
-def relu(prime: bool, in_ft: np.array) -> np.array:
-    ''' ReLU: f(x) = max(0, x) '''
+def relu(prime: bool, in_ft: np.array, fw_out: np.array=None) -> np.array:
+    ''' ReLU:
+        f(x) = max(0, x)
+        f'(x) = (0 if x <= 0) , (1 if x > 0)
+    '''
     if not prime:
         return np.maximum(0, in_ft)
     else:
-        pass
+        rc = 0 if in_ft <= 0 else 1
+        return rc
 
-def softplus(prime: bool, in_ft: np.array) -> np.array:
-    ''' SoftPlus: f(x) = ln(1 + e^x) '''
-    if not prime:
-        return np.log(1 + np.exp(in_ft))
-    else:
-        pass
-
-def sigmoid(prime: bool, in_ft: np.array) -> np.array:
-    ''' Sigmoid: f(x) = 1 / 1 + e^(-x) '''
-    if not prime:
-        return 1 / (1 + np.exp(-in_ft))
-    else:
-        pass
-
-
-def softmax(prime: bool, in_ft: np.array) -> np.array:
+def softplus(prime: bool, in_ft: np.array, fw_out: np.array=None) -> np.array:
+    ''' SoftPlus:
+        f(x) = ln(1 + e^x)
+        f'(x) = e^x / 1 + e^x
     '''
-        Softmax: f(x) = e^x / SUMy->N(e^y), where N is the number of vector elements
+    e_x = np.exp(in_ft)
+    if not prime:
+        return np.log(1 + e_x)
+    else:
+        return e_x / (1 + e_x)
+
+def sigmoid(prime: bool, in_ft: np.array, fw_out: np.array=None) -> np.array:
+    ''' Sigmoid:
+        f(x) = 1 / 1 + e^(-x)
+        f'(x) = (1 / 1 + e^(-x)) * (1 - (1 / 1 + e^(-x)))
+    '''
+    sig = 1 / (1 + np.exp( - in_ft))
+    if not prime:
+        return sig
+    else:
+        return sig * (1 - sig)
+
+def softmax(prime: bool, in_ft: np.array, fw_out: np.array=None) -> np.array:
+    '''
+        Softmax:
+        f(x) = e^x / SUMy->N(e^y), where N is the number of vector elements
         Details with "axis = 0" and using the np.max in nominator
         used based on discussion here -
         https://stackoverflow.com/questions/34968722/how-to-implement-the-softmax-function-in-python
+
+        Output of forward softmax is needed to compute the input gradient
+        f'(x) inspired by and understood with help of:
+            https://e2eml.school/softmax.html
+            https://gitlab.com/brohrer/cottonwood/
     '''
     if not prime:
         e_x = np.exp(in_ft - np.max(in_ft))
         return e_x / e_x.sum(axis = 0, keepdims = True)
     else:
-        pass
+        # Create two 2D arrays with 1 row to create a Jacobian matrix
+        fw_out = np.reshape(fw_out, (1, -1))
+        grad = np.reshape(in_ft, (1, -1))
 
+        d_softmax = (
+            fw_out * np.identity(fw_out.size)
+            - np.dot(fw_out.T, fw_out))
+        
+        return np.dot(grad, d_softmax).ravel()
+
+def test_softmax(in_ft: np.array, test_num: int):
+    # TODO: test softmax derivative too
+    # 6 decimals provide same results, after 7th decimal assertion does not pass
+    ROUND_DECIMALS = 6
+
+    # Calculate ours
+    res = softmax(prime=False, in_ft=in_ft)
+    res = np.round(res, ROUND_DECIMALS) # Our result
+
+    # Calculate theirs
+    in_ft_t = torch.tensor(in_ft).float() # Same list converted to tensor for torch
+    check = torch.nn.functional.softmax(in_ft_t, dim=0).numpy()
+    check = np.round(check.tolist(), ROUND_DECIMALS) # PyTorch result
+
+    assert np.array_equal(res, check), f"Softmax {test_num} failed \n Ours:   {res} \
+                                        \n Theirs: {check}"
+
+
+if __name__ == "__main__":
+    # Check softmax forward propagation with results from PyTorch as baseline
+    ### Case 1
+    in_ft = [1, 3, 2]
+    test_softmax(in_ft, 1)
+    
+    ### Case 2
+    in_ft = [5, 2, 8, 9]
+    test_softmax(in_ft, 2)
--- 1-fnn/mlp-components/fc.py
+++ 1-fnn/mlp-components/fc.py
@@ -7,12 +7,19 @@ from collections.abc import Callable
 # In here, in addition to dL/dW discussed in lecture slides, I understood what is actually
 # propagated to the next layer, that is the derivative of th error multiplied by the weights
 #
-#
 
 
 class FC:
     """
     Fully connected layer implementation - Ivan Turasov
+
+    In this implementation the activation function is a part of the FC layer,
+    rather than a separate layer. Hence it is called as part of fw and bw
+    pass of the FC layer.
+    This choice was made due to the explanation in lectures, and even though
+    it brings another layer of complexity when other types of layers would be
+    implemented, for this particular assignment this seemed the best ways to
+    fully understand the full backward propagation.
     """
 
     total_fc_num = 0
@@ -35,31 +42,37 @@ class FC:
         self.wm = np.random.default_rng(1).random((num_in_ft, num_out_ft))
         self.bs = np.zeros(1, num_out_ft)
         self.input = None
-        self.out_before_activation = None
+        self.logits = None
         self.output = None
 
-    def fw(self, in_ft: np.array) -> np.array:
+    def fw(self, fw_in: np.array) -> np.array:
         """Apply weights and activation function"""
-        self.input = in_ft
-        self.out_before_activation = np.dot(in_ft, self.wm) + self.bs # Yin
-        self.output = self.activation(prime=False, in_ft=self.out_before_activation)
-        return self.output
+        self.input = fw_in
+        self.logits = np.dot(fw_in, self.wm) + self.bs # Yin
+        self.fw_out = self.activation(prime=False,
+                                      in_ft=self.logits)
+        return self.fw_out
 
-    def bw(self, in_err: np.array, learning_rate: float) -> np.array:
+    def bw(self, bw_in: np.array, learning_rate: float) -> np.array:
         """
         Calculate error based on error of the previous layer
-        activation_prime(self.out_before_activation)
+        activation_prime(self.logits)
         """
 
         # Activation derivative dY/dYin(Yin)
-        activation_err = self.activation(prime=True, in_ft=self.out_before_activation)
+        activation_err = self.activation(prime=True,
+                                         in_ft=self.logits, fw_out=self.fw_out)
 
         # Loss derivative over weights (dL/dW)
-        weight_err = np.dot(self.input.T, in_err * activation_err)
+        weight_err = np.dot(self.input.T, bw_in * activation_err)
+
         # Loss Derivative over previous layer input (dL/din_ft)
-        out_err = np.dot(in_err * activation_err, self.wm.T)
+        # Multiplying by wight matrix, because the derivative is wrt in_ft
+        # and by chain rule, the last term is d(out_before_act)/din_ft = weights
+        bw_out = np.dot(bw_in * activation_err, self.wm.T)
+
         # Update weights (w1 = w0 - n * dL/dW) - after the old weight matrix was used
         self.wm = self.wm - learning_rate * weight_err
 
         # Mod 4 slide 80
-        return out_err  # Propagated error to next layer
+        return bw_out  # Propagated error to next layer
