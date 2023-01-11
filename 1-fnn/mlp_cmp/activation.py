1mlp: Fix without softmax; Separate activation layer

1-fnn/mlp_cmp/activation.py
@@ -53,23 +53,19 @@ def softmax(prime: bool, in_ft: np.array, fw_out: np.array=None) -> np.array:
     softmax = e / e.sum(axis=0, keepdims=True)
     if not prime:
         return softmax
-    # else:
-    #     SM = in_ft.reshape((-1, 1))
-    #     return np.diagflat(in_ft) - np.dot(SM, SM.T)
-    else:
-        I = np.eye(in_ft.shape[0])
-        ret = softmax * (I - softmax.T)
-        # print(ret)
-        return ret
-        # # Create two 2D arrays with 1 row to create a Jacobian matrix
-        # fw_out = np.reshape(fw_out, (1, -1))
-        # grad = np.reshape(in_ft, (1, -1))
 
-        # d_softmax = (
-        #     fw_out * np.identity(fw_out.size)
-        #     - np.dot(fw_out.T, fw_out))
-    
-        # return np.dot(grad, d_softmax).ravel()
+    else:
+        grad = np.zeros((1, len(fw_out[0])))
+        # print(fw_out)
+        # print(grad)
+        for i in range(len(fw_out)):
+            for j in range(len(fw_out)):
+
+                if i == j:
+                    grad[i] = grad[i] + (fw_out[i] * (1 - fw_out[i]))
+                else: 
+                    grad[i] = grad[i] + (-1 * fw_out[i] * fw_out[j])
+        return grad
 
 def test_softmax(in_ft: np.array, test_num: int):
     # TODO: test softmax derivative too

1-fnn/mlp_cmp/fc.py
@@ -29,7 +29,6 @@ class FC:
         self,
         num_in_ft: int,
         num_out_ft: int,
-        activation: Callable[[bool, np.array], np.array],
     ):
         """
         wm - weight matrix of size (num_in_ft x num_out_ft)
@@ -40,9 +39,11 @@ class FC:
         self.seq_num = FC.total_fc_num
         self.num_in_ft = num_in_ft
         self.num_out_ft = num_out_ft
-        self.activation = activation  # Activation function
-        self.wm = np.random.default_rng(1).random((num_in_ft, num_out_ft)) - 0.7
-        self.bs = np.zeros((1, num_out_ft))
+        # self.activation = activation  # Activation function
+        # self.wm = np.random.default_rng(1).random((num_in_ft, num_out_ft)) - 0.7
+        self.wm = np.random.rand(num_in_ft, num_out_ft) - 0.5
+        # self.bs = np.zeros((1, num_out_ft))
+        self.bs = np.random.rand(1, num_out_ft) - 0.5
         self.input = None
         self.logits = None
         self.output = None
@@ -53,8 +54,8 @@ class FC:
     def fw(self, fw_in: np.array) -> np.array:
         """Apply weights and activation function"""
         self.input = fw_in
-        self.logits = np.dot(fw_in, self.wm) + self.bs  # Yin
-        self.fw_out = self.activation(prime=False, in_ft=self.logits)
+        self.fw_out = np.dot(fw_in, self.wm) + self.bs  # Yin
+        # self.fw_out = self.activation(prime=False, in_ft=self.logits)
         return self.fw_out
 
     def bw(self, bw_in: np.array, learning_rate: float) -> np.array:
@@ -62,25 +63,18 @@ class FC:
         Calculate error based on error of the previous layer
         activation_prime(self.logits)
         """
-
         # Activation derivative dY/dYin(Yin)
-        activation_err = self.activation(prime=True,
-                                            in_ft=self.logits,
-                                            fw_out=self.fw_out) * bw_in
-        if self.activation == a.softmax:
-            activation_err = np.sum(activation_err, axis=0).reshape((-1, 1)).T
-
         # Loss derivative over weights (dL/dW)
-        weight_err = np.dot(self.input.T, activation_err)
+        weight_err = np.dot(self.input.T, bw_in)
 
         # Loss Derivative over previous layer input (dL/din_ft)
         # Multiplying by wight matrix, because the derivative is wrt in_ft
         # and by chain rule, the last term is d(out_before_act)/din_ft = weights
-        bw_out = np.dot(activation_err, self.wm.T)
+        bw_out = np.dot(bw_in, self.wm.T)
 
         # Update weights (w1 = w0 - n * dL/dW) - after the old weight matrix was used
         self.wm = self.wm - learning_rate * weight_err
-        self.bs = self.bs - learning_rate * activation_err
+        self.bs = self.bs - learning_rate * bw_in
 
         # Mod 4 slide 80
         return bw_out  # Propagated error to next layer

1-fnn/mlp_cmp/loss.py
@@ -15,10 +15,17 @@ def ce_loss(prime: bool, p: np.array, t: np.array):
     '''
     # Since both target and prediction are np arrays, we can compute
     # Hadamard product of them and get a sum of elements after
-    p = p.clip(min=1e-8, max=None)
+    # p = p.clip(min=1e-8, max=None)
     if not prime:
         loss = -np.sum(t * np.log(p))
     else:
         loss = t * (-1 / p)
-
     return loss
+
+# loss function and its derivative
+def mse(prime: bool, p: np.array, t: np.array):
+    if not prime:
+        return np.mean(np.power(t - p, 2))
+        # return np.mean(np.power(t - p, 2))
+    else:
+        return 2*(p - t)/t.size

1-fnn/mlp_cmp/mlp.py
@@ -47,15 +47,20 @@ class MLP:
     ):
 
         num_s = len(train_samples)
-        batch_loss = 0
-        loss_curve = []
+        accuracy_sum = 0
 
         for e in tqdm(range(epochs)):
             err = 0
+            total_p = 0
+            correct_p = 0
             if self.gd_type == "stoc":
                 for i in range(num_s):
-                    p = self.predict(train_samples[i])
+                    total_p += 1
+                    p = train_samples[i].reshape(len(train_samples[i]), 1).T
+                    p = self.predict(p)
                     t = train_labels[i]
+                    if np.argmax(p) == np.argmax(t):
+                        correct_p += 1
 
                     err += self.loss(prime=False, p=p, t=t)
                     gradient = self.loss(prime=True, p=p, t=t)
@@ -64,16 +69,19 @@ class MLP:
                         gradient = l.bw(gradient, l_rate)
                         # print(gradient)
                 err /= num_s
-                loss_curve.append(err)
+
             elif self.gd_type == "bat":
                 gradient = 0
 
                 # Forward - accumulate loss and gradient of all training set
                 for i in range(num_s):
+                    total_p += 1
                     # Go through all FC layers
                     p = self.predict(train_samples[i])
                     t = train_labels[i]
 
+                    if np.argmax(p) == np.argmax(t):
+                        correct_p += 1
                     err += self.loss(prime=False, p=p, t=t)
                     gradient += self.loss(prime=True, p=p, t=t)
                 err /= num_s
@@ -83,12 +91,12 @@ class MLP:
                     # Weights are updated in every layer, so the
                     # end gradient is not needed
                     gradient = l.bw(gradient, l_rate)
-
-            if e%100 == 0:
-                    print(f"{self.gd_type}: Epoch #{e} - avg CE = {err}")
-        plt.plot(loss_curve)
-        plt.show()
-
+            accuracy_sum += correct_p / total_p
+            if e%10 == 0:
+                print(f"{self.gd_type}: Epoch #{e} - avg loss = {err}"
+                      f", avg Accuracy = {accuracy_sum / (e + 1)}"
+                      f", C: {correct_p}, T: {total_p}")
+        print(f"Total mean accuracy during training {accuracy_sum / epochs}")
 
     def validate():
         pass
@@ -99,10 +107,16 @@ class MLP:
         test_labels: np.array,
     ):
         err = 0
+        total = 0
+        correct = 0
         for i, s in enumerate(test_samples):
+            total += 1
             p = self.predict(s)
             t = test_labels[i]
-            p_class = np.argmax(p) # index of prediction
-            t_class = np.argmax(t) # index of label
+            # p_class = np.argmax(p) # index of prediction
+            # t_class = np.argmax(t) # index of label
+            if np.argmax(p) == np.argmax(t):
+                correct += 1
             err += self.loss(prime=False, p=p, t=t)
+        print(f"Accuracy = {correct} / {total} = {correct / total}")
         print(f"Avg test err = {err / len(test_samples)}")